26d13c404fc6b07d1ba38877e67259cbff0fd286
Daniela Silva <daniela92_@hotmail.de>
2026-03-16 14:53:29 +0100

    Update base path in Vite configuration to '/hotelvelastay/'

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/hotelvelareact/',
+  base: '/hotelvelastay/',
   server: {
     port: 5173,
     open: true,
